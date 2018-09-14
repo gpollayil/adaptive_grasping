27dc8511e344f01e7174c432b69f8892d11154cc
George Jose Pollayil <gpollayil@gmail.com>
2018-09-14 11:24:49 +0200

    Loading params for frame names and publishing inside while

--- src/tf_listener.cpp
+++ src/tf_listener.cpp
@@ -21,6 +21,8 @@
 // GLOBAL VARIABLES
 tf::StampedTransform transform;             // for storing the listened tf
 ros::Publisher pub_tf_info; 								// publisher for transformed tf
+std::string child_frame;
+std::string parent_frame;
 
 // AUX FUNCTION
 void transform_and_publish(const tf::StampedTransform& trans) {
@@ -36,8 +38,12 @@ int main(int argc, char** argv){
 	ros::init(argc, argv, "tf_listen_publish_node");
 	ros::NodeHandle tflp_nh;
 
+  // Setting needed params from param server
+	tflp_nh.param<std::string>("child_frame", child_frame, "/right_hand_ee_link");
+	tflp_nh.param<std::string>("parent_frame", parent_frame, "/world");
+
   // Creating a ROS publisher for the output
-	pub_tf_info = tflp_nh.advertise<geometry_msgs::PoseStamped>("output_topic", 1);
+	pub_tf_info = tflp_nh.advertise<geometry_msgs::TransformStamped>("output_topic", 1);
 
 	// Creating a TF listener and listening
   tf::TransformListener tf_listener;
@@ -45,14 +51,14 @@ int main(int argc, char** argv){
 
   while (tflp_nh.ok()){
     try{
-      tf_listener.lookupTransform("/right_hand_ee_link", "/world", ros::Time(0), transform);
+      tf_listener.waitForTransform(child_frame, parent_frame, ros::Time(0), ros::Duration(10.0) );
+      tf_listener.lookupTransform(child_frame, parent_frame, ros::Time(0), transform);
     } catch (tf::TransformException ex){
       ROS_ERROR("%s", ex.what());
       ros::Duration(1.0).sleep();
     }
+    transform_and_publish(transform);
   }
-
-  transform_and_publish(transform);
 
 	// Success message
 	std::cout << "TF Listener started to work! The TF info are being published" << std::endl;
