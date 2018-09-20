c2f265350f37d95f27bc0001ec3c0db819669548
George Jose Pollayil <gpollayil@gmail.com>
2018-09-20 17:17:29 +0200

    Removed the parameter parsing to the adaptiveGrasper class and made the handleCollsion and getTransforms private

--- src/contactState.cpp
+++ src/contactState.cpp
@@ -8,40 +8,17 @@
 using namespace adaptive_grasping;
 
 /* CONSTRUCTOR */
-contactState::contactState(){
-  // Getting params from parameter server
-  std::string topic_name = "finger_collision_topic";
-  std::string default_topic_name = "/touching_finger_topic";
-  if(!getParamOfYaml(topic_name, default_topic_name)){
-    ROS_WARN_STREAM(topic_name << " not found! Please load the correct one from yaml!");
-  }
-
+contactState::contactState(std::string topic_name){
   // Subscribing to topic
   finger_col_sub = node_contact_state.subscribe(topic_name, 1000, &contactState::handleCollision, this);
 }
 
 /* HANDLECOLLISION */
 void contactState::handleCollision(const std_msgs::Int8::ConstPtr& msg){
-
-}
-
-/* GETPARAMSOFYAML */
-bool contactState::getParamOfYaml(std::string param_name, std::string default_param_name){
-  bool success = true;
-
-  // Looking if parameter exists and loading it
-  if(!ros::param::get(std::string(EXEC_NAMESPACE) +
-      std::string(CLASS_NAMESPACE) + param_name, this->temp_param)){
-        ROS_WARN_STREAM(param_name << " param not found in param server! Using default.");
-
-        // Saving default
-        this->temp_param = default_param_name;
-
-        success = false;
-	}
-
-  // Inserting in map
-  params_map[param_name] = this->temp_param;
-
-  return success;
+  // Trying to save msg  with and empty tuple (if already touching finger, then
+  // won't insert)
+  touching_finger = msg->data;
+  Eigen::Affine3d
+  std::tuple<std::string, Eigen::Affine3d,
+    Eigen::Affine3d> empty (std::make_tuple(20,'b'))
 }
