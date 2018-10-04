5e6c8d25f06f675a302047e31a8e2a92dce0fe9d
George Jose Pollayil <gpollayil@gmail.com>
2018-10-04 11:43:12 +0200

    Started defining the functions of robotCommander

---
+++ src/robotCommander.cpp
@@ -0,0 +1,17 @@
+#include "robotCommander.h"
+
+#define EXEC_NAMESPACE    "adaptive_grasping"
+#define CLASS_NAMESPACE   "robot_commander"
+#define DEBUG             1   // print out additional info
+
+/**
+* @brief The following are functions of the class robotCommander.
+*
+*/
+
+using namespace adaptive_grasping;
+
+/* CONSTRUCTOR */
+robotCommander::robotCommander(std::string hand_topic_, std::string arm_topic_){
+  
+}
