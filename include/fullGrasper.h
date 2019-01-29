d0e4b7d5f88300b19252fe6f1f512923c1978ec9
George Jose Pollayil <gpollayil@gmail.com>
2019-01-29 18:29:09 +0100

    Implementing the switching of the controller in fullGrasper

--- include/fullGrasper.h
+++ include/fullGrasper.h
@@ -3,6 +3,8 @@
 
 #include "adaptiveGrasper.h"            // Most of other h files are included in this one
 
+#include <controller_manager_msgs/SwitchController.h>
+
 /**
 * @brief This class is created by the main of the full_grasping_node: it has inside it
 * all the other classes: contact_state, matrix_creator, contact_preserver and adaptive_grasper.
@@ -25,6 +27,16 @@ namespace adaptive_grasping {
         */
         fullGrasper();
 
+        /** OVERLOADED CONSTRUCTOR
+        * @brief Default constructor for fullGrasper
+        *
+        * @param robot_ns_ a vector containing the strings of the namespaces of robots (kuka and hand?)
+        * @param normal_controllers_names_ a vector containing the strings of the normal controllers
+        * @param velocity_controllers_names_ a vector containing the strings of the velocity controllers
+        * @return null
+        */
+        fullGrasper(std::vector<std::string> robot_ns_, std::vector<std::string> normal_controllers_names_, std::vector<std::string> velocity_controllers_names_);
+
         /** INITIALIZE
         * @brief Public function for initializing the object (recalls the initialize function of adaptive_grasper)
         *
@@ -33,6 +45,16 @@ namespace adaptive_grasping {
         */
         bool initialize(std::vector<std::string> param_names);
 
+        /** SWITCHCONTROL
+        * @brief Public function for switching from a controller to another
+        *
+        * @param robot_name containing the name of robot
+        * @param from_controller containing the name of controller to be stopped
+        * @param to_controller containing the name of controller to be started
+        * @return bool = true if success
+        */
+        bool switch_control(std::string robot_name, std::string from_controller, std::string to_controller);
+
         private:
 
         // The main element of the object
@@ -40,6 +62,19 @@ namespace adaptive_grasping {
 
         // The velocity command to be sent to the robot commander (using the one of adaptive_grasper)
         velCommand handclose_command;
+
+        // The namespace of the robot controllers (in order kuka and soft_hand)
+        std::vector<std::string> robot_namespaces;
+
+        // The switch controller service name
+        std::string switch_service_name = "/controller_manager/switch_controller";
+
+        // The names of the normal controllers and the velocity controllers (in order kuka and soft_hand)
+        std::vector<std::string> normal_controllers_names;
+        std::vector<std::string> velocity_controllers_names;
+
+        // A controller_mangager msg for switching controllers
+        controller_manager_msgs::SwitchController switch_controller;
 
     };
 
--- src/fullGrasper.cpp
+++ src/fullGrasper.cpp
@@ -17,9 +17,40 @@ fullGrasper::fullGrasper(){
     // Nothing to do here
 }
 
+/* OVERLOADED CONSTRUCTOR */
+fullGrasper::fullGrasper(std::vector<std::string> robot_ns_, std::vector<std::string> normal_controllers_names_, std::vector<std::string> velocity_controllers_names_){
+    // Setting the robot controllers namespace
+    this->robot_namespaces = robot_ns_;
+
+    // Setting the names of the controllers
+    this->normal_controllers_names = normal_controllers_names_;
+    this->velocity_controllers_names = velocity_controllers_names_;
+}
+
 /* INITIALIZE */
 bool fullGrasper::initialize(std::vector<std::string> param_names){
     // Using the initialize of adaptiveGrasper
     return this->adaptive_grasper.initialize(param_names);
 }
+
+/* SWITCHCONTROL */
+bool fullGrasper::switch_control(std::string robot_name, std::string from_controller, std::string to_controller){
+    // Temporary bool to be returned
+    bool success = false;
+
+    // Clearing the switch message
+    this->switch_controller.request.start_controllers.clear();
+    this->switch_controller.request.stop_controllers.clear();
+
+    // Filling up the switch message
+    this->switch_controller.request.start_controllers.push_back(to_controller);
+    this->switch_controller.request.stop_controllers.push_back(from_controller);
+    this->switch_controller.request.strictness = controller_manager_msgs::SwitchController::Request::STRICT;
+
+    // Swithching controller by calling the service
+    ros::service::call<controller_manager_msgs::SwitchController>(robot_name + this->switch_service_name, this->switch_controller);
+
+
+}
+
 
