de4fed56ab3e39bfc64417ff14d13311159edb8a
mpollayil <mathewjosepollayil@gmail.com>
2019-03-22 12:06:22 +0100

    Starting to add full task routine to fullGrasper

--- include/fullGrasper.h
+++ include/fullGrasper.h
@@ -1,15 +1,32 @@
 #ifndef FULL_GRASPER_H
 #define FULL_GRASPER_H
 
-#include "adaptiveGrasper.h"            // Most of other h files are included in this one
-
+// Basic includes
+#include <ros/service.h>
 #include <controller_manager_msgs/SwitchController.h>
+#include <eigen_conversions/eigen_msg.h>
+
+// ROS msg includes
+#include <geometry_msgs/Pose.h>
+#include "std_srvs/SetBool.h"
+
+// Custom msg and srv includes
+#include "adaptive_grasping/set_object.h"
+#include "panda_softhand_control/hand_control.h"
+#include "panda_softhand_control/joint_control.h"
+#include "panda_softhand_control/pose_control.h"
+#include "panda_softhand_control/slerp_control.h"
+
+// Custom Includes
+#include "adaptiveGrasper.h"            // Most of other h files are included in this one
 
 /**
 * @brief This class is created by the main of the full_grasping_node: it has inside it
 * all the other classes: contact_state, matrix_creator, contact_preserver and adaptive_grasper.
 * The purpouse of this node is to start closing the hand until a touch is detected; then the
-* callback of the adaptive_grasper is called
+* callback of the adaptive_grasper is called.
+* It also has a callback that uses panda_softhand_control package to perform a full adaptive
+* grasping task, lift and handover.
 *
 */
 
@@ -38,6 +55,22 @@ namespace adaptive_grasping {
         */
         fullGrasper(std::string arm_ns_, std::string hand_ns_, std::vector<std::string> normal_controllers_names_, std::vector<std::string> velocity_controllers_names_);
 
+        /** PARSETASKPARAMS
+        * @brief Public function for parsing required parameters for full grasping routine
+        *
+        * @param null
+        * @return null
+        */
+        bool parse_task_params();
+
+        /** CONVERTVECTORTOPOSE
+        * @brief Public function for converting xyzrpy vector to geometry_msgs Pose
+        *
+        * @param input_vec std vector xyzrpy
+        * @return pose geometry_msgs::Pose
+        */
+        geometry_msgs::Pose convert_vector_to_pose(std::vector<double> input_vec);
+
         /** INITIALIZE
         * @brief Public function for initializing the object (recalls the initialize function of adaptive_grasper)
         *
@@ -64,10 +97,37 @@ namespace adaptive_grasping {
         */
         void spin();
 
+        /** GETOBJECTPOSE
+        * @brief Callback function for getting current object pose
+        *
+        * @param msg pose
+        * @return null
+        */
+        void get_object_pose(const geometry_msgs::Pose::ConstPtr &msg);
+
+        /** CALLSETOBJECT
+        * @brief Callback function for setting the particular object to be grasped
+        *
+        * @param msg pose
+        * @return null
+        */
+        bool call_set_object(adaptive_grasping::set_object::Request &req, adaptive_grasping::set_object::Response &res);
+
+        /** CALLADAPTIVEGRASPTASK
+        * @brief Callback function for performing the whole adaptive grasp routine
+        *
+        * @param req / res
+        * @return bool success
+        */
+        bool call_adaptive_grasp_task(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res);
+
         // The main element of the object
         adaptiveGrasper adaptive_grasper;
 
         private:
+
+        // Rose main variables
+        ros::NodeHandle nh;
 
         // The velocity command to be sent to the robot commander (using the one of adaptive_grasper)
         velCommand handclose_command;
@@ -85,6 +145,32 @@ namespace adaptive_grasping {
 
         // A controller_mangager msg for switching controllers
         controller_manager_msgs::SwitchController switch_controller;
+
+        // Subscriber to object pose and the pose
+        ros::Subscriber object_sub;
+        geometry_msgs::Pose object_pose_T;
+
+        // Service Servers
+        ros::ServiceServer adaptive_task_server;
+        ros::ServiceServer set_object_server;
+
+        // The XmlRpc value for parsing complex params
+        XmlRpc::XmlRpcValue task_seq_params;
+
+        // Parsed task sequence variables
+        std::string robot_name;                     // Name of the robot (namespace)
+        std::string robot_joints_name;              // Name of the robot joints (without the number of the joints)
+        std::string pos_controller;                 // Name of position controller
+        std::string imp_controller;                 // Name of impedance controller
+        std::vector<double> home_joints;
+        std::vector<double> grasp_transform;
+        geometry_msgs::Pose grasp_T;
+        std::vector<double> pre_grasp_transform;
+        geometry_msgs::Pose pre_grasp_T;
+        std::vector<double> handover_joints;
+        double handover_thresh;
+
+        std::map<std::string, std::vector<double>> poses_map;     // The map containing the notable poses
 
     };
 
--- src/fullGrasper.cpp
+++ src/fullGrasper.cpp
@@ -1,4 +1,5 @@
 #include "fullGrasper.h"
+#include "utils/parsing_utilities.h"
 
 #define EXEC_NAMESPACE    "adaptive_grasping"
 #define CLASS_NAMESPACE   "full_grasper"
@@ -19,6 +20,11 @@ fullGrasper::fullGrasper(){
 
 /* OVERLOADED CONSTRUCTOR */
 fullGrasper::fullGrasper(std::string arm_ns_, std::string hand_ns_, std::vector<std::string> normal_controllers_names_, std::vector<std::string> velocity_controllers_names_){
+    // Parsing the task params
+    if(!this->parse_task_params()){
+        ROS_ERROR("The parsing of task parameters went wrong. Be careful, using default values...");
+    }
+    
     // Setting the robot controllers namespace
     this->arm_namespace = arm_ns_;
     this->hand_namespace = hand_ns_;
@@ -26,6 +32,121 @@ fullGrasper::fullGrasper(std::string arm_ns_, std::string hand_ns_, std::vector<
     // Setting the names of the controllers
     this->normal_controllers_names = normal_controllers_names_;
     this->velocity_controllers_names = velocity_controllers_names_;
+
+    // Initializing the object subscriber and waiting (TODO: parse the topic name)
+    this->object_sub = this->nh.subscribe("/object_pose", 1, &fullGrasper::get_object_pose, this);
+    ros::topic::waitForMessage<geometry_msgs::Pose>("/object_pose", ros::Duration(2.0));
+}
+
+/* PARSETASKPARAMS */
+bool fullGrasper::parse_task_params(){
+    bool success = true;
+
+    if(!ros::param::get("/task_sequencer/robot_name", this->robot_name)){
+		ROS_WARN("The param 'robot_name' not found in param server! Using default.");
+		this->robot_name = "panda_arm";
+		success = false;
+	}
+
+    if(!ros::param::get("/task_sequencer/robot_joints_name", this->robot_joints_name)){
+		ROS_WARN("The param 'robot_joints_name' not found in param server! Using default.");
+		this->robot_joints_name = "panda_joint";
+		success = false;
+	}
+
+    if(!ros::param::get("/task_sequencer/pos_controller", this->pos_controller)){
+		ROS_WARN("The param 'pos_controller' not found in param server! Using default.");
+		this->pos_controller = "position_joint_trajectory_controller";
+		success = false;
+	}
+
+    if(!ros::param::get("/task_sequencer/imp_controller", this->imp_controller)){
+		ROS_WARN("The param 'imp_controller' not found in param server! Using default.");
+		this->imp_controller = "cartesian_impedance_controller_softbots_stiff_matrix";
+		success = false;
+	}
+
+	if(!ros::param::get("/task_sequencer/home_joints", this->home_joints)){
+		ROS_WARN("The param 'home_joints' not found in param server! Using default.");
+		this->home_joints = {-0.035, -0.109, -0.048, -1.888, 0.075, 1.797, -0.110};
+		success = false;
+	}
+
+    if(!ros::param::get("/task_sequencer/grasp_transform", this->grasp_transform)){
+		ROS_WARN("The param 'grasp_transform' not found in param server! Using default.");
+		this->grasp_transform.resize(6);
+        std::fill(this->grasp_transform.begin(), this->grasp_transform.end(), 0.0);
+		success = false;
+	}
+
+    // Converting the grasp_transform vector to geometry_msgs Pose
+    this->grasp_T = this->convert_vector_to_pose(this->grasp_transform);
+
+    if(!ros::param::get("/task_sequencer/pre_grasp_transform", this->pre_grasp_transform)){
+		ROS_WARN("The param 'pre_grasp_transform' not found in param server! Using default.");
+		this->pre_grasp_transform.resize(6);
+        std::fill(this->pre_grasp_transform.begin(), this->pre_grasp_transform.end(), 0.0);
+		success = false;
+	}
+
+    // Converting the pre_grasp_transform vector to geometry_msgs Pose
+    this->pre_grasp_T = this->convert_vector_to_pose(this->pre_grasp_transform);
+
+    if(!ros::param::get("/task_sequencer/handover_joints", this->handover_joints)){
+		ROS_WARN("The param 'handover_joints' not found in param server! Using default.");
+		this->handover_joints = {-0.101, 0.161, 0.159, -1.651, 2.023, 2.419, -0.006};
+		success = false;
+	}
+
+    if(!ros::param::get("/task_sequencer/handover_thresh", this->handover_thresh)){
+		ROS_WARN("The param 'handover_thresh' not found in param server! Using default.");
+		this->handover_thresh = 4.5;
+		success = false;
+	}
+
+    // Getting the XmlRpc value and parsing
+    if(!ros::param::get("/task_sequencer", this->task_seq_params)){
+        ROS_ERROR("Could not get the XmlRpc value.");
+        success = false;
+    }
+
+    if(!parseParameter(this->task_seq_params, this->poses_map, "poses_map")){
+        ROS_ERROR("Could not parse the poses map.");
+        success = false;
+    }
+
+    if(DEBUG){
+        ROS_INFO_STREAM("The poses map is");
+        for(auto it : this->poses_map){
+            std::cout << it.first << " : [ ";
+            for(auto vec_it : it.second){
+                std::cout << vec_it << " ";
+            } 
+            std::cout << "]" << std::endl;     
+        }
+    }
+
+    return success;
+}
+
+/* CONVERTVECTORTOPOSE */
+geometry_msgs::Pose fullGrasper::convert_vector_to_pose(std::vector<double> input_vec){
+    
+    // Creating temporary variables
+    geometry_msgs::Pose output_pose;
+    Eigen::Affine3d output_affine;
+
+    // Getting translation and rotation
+    Eigen::Vector3d translation(input_vec[0], input_vec[1], input_vec[2]);
+    output_affine.translation() = translation;
+    Eigen::Matrix3d rotation = Eigen::Matrix3d(Eigen::AngleAxisd(input_vec[5], Eigen::Vector3d::UnitZ())
+        * Eigen::AngleAxisd(input_vec[4], Eigen::Vector3d::UnitY())
+        * Eigen::AngleAxisd(input_vec[3], Eigen::Vector3d::UnitX()));
+    output_affine.linear() = rotation;    
+    
+    // Converting to geometry_msgs and returning
+    tf::poseEigenToMsg(output_affine, output_pose);
+    return output_pose;
 }
 
 /* INITIALIZE */
@@ -62,3 +183,32 @@ void fullGrasper::spin(){
     this->adaptive_grasper.spinGrasper();
 }
 
+/* GETOBJECTPOSE */
+void fullGrasper::get_object_pose(const geometry_msgs::Pose::ConstPtr &msg){
+
+    // Saving the message
+    this->object_pose_T = *msg;
+}
+
+/* CALLSETOBJECT */
+bool fullGrasper::call_set_object(adaptive_grasping::set_object::Request &req, adaptive_grasping::set_object::Response &res){
+
+    // Checking if the parsed map contains the requested object
+    auto search = this->poses_map.find(req.object_name);
+    if(search == this->poses_map.end()){
+        ROS_WARN_STREAM("The object " << req.object_name << " is not present in my memory; using the previously used one or default... Did you spell it correctly? Is it in the yaml?");
+        res.result = false;
+        return res.result;
+    }
+
+    // Setting the grasp pose as requested
+    this->grasp_transform = this->poses_map.at(req.object_name);
+
+    // Converting the grasp_transform vector to geometry_msgs Pose
+    this->grasp_T = this->convert_vector_to_pose(this->grasp_transform);
+
+    // Now, everything is ok
+    ROS_INFO_STREAM("Grasp pose changed. Object set to " << req.object_name << ".");
+    res.result = true;
+    return res.result;
+}
