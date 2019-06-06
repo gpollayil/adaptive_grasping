3f2e127e0bb698d776e19b01eee76a6e6dec7654
gpollayil <gpollayil@gmail.com>
2019-06-06 15:28:57 +0200

    Added the test file; but core dumped for now

--- test/test_reverse_priority.cpp
+++ test/test_reverse_priority.cpp
@@ -22,8 +22,59 @@ int main(int argc, char **argv) {
 
     // Creating the objects
     std::vector<basicTask> task_vec;
-    reversePriorityManager rp_manager;
+    task_vec.clear();
+    reversePriorityManager rp_manager(13, 0.0, 0.01);
 
+    // Temporary variables
+    basicTask tmp_task;
+    Eigen::VectorXd tmp_x_dot;
+    Eigen::MatrixXd tmp_jac;
+    int tmp_priority;
 
+    // First task
+    tmp_x_dot.resize(7);
+    tmp_x_dot << -1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0;
+    tmp_jac.resize(7, 10);
+    tmp_jac << 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+            1.5, 1.5, 0.0, 0.0, 1.0, 0.0, 1.5, -1.0, 0.0, 0.0,
+            -1.0, 0.0, 0.0, 0.0, 0.0, 1.0, -1.0, 0.0, -1.0, 0.5,
+            1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, -1.0;
+    tmp_priority = 1;
 
+    tmp_task.set_task_x_dot(tmp_x_dot);
+    tmp_task.set_task_jacobian(tmp_jac);
+    tmp_task.set_task_priority(tmp_priority);
+
+    task_vec.push_back(tmp_task);
+
+    // Second task
+    tmp_x_dot.resize(7);
+    tmp_x_dot << -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0;
+    tmp_jac.resize(7, 10);
+    tmp_jac << Eigen::MatrixXd::Identity(7, 10);
+    tmp_priority = 2;
+
+    tmp_task.set_task_x_dot(tmp_x_dot);
+    tmp_task.set_task_jacobian(tmp_jac);
+    tmp_task.set_task_priority(tmp_priority);
+
+    task_vec.push_back(tmp_task);
+
+    // Filling up RP Manager
+    rp_manager.clear_set();
+    rp_manager.insert_tasks(task_vec);
+
+    // Print out of the task set
+    rp_manager.print_set();
+
+    // Computing and couting the RP solution
+    Eigen::VectorXd x_ref;
+    if(rp_manager.solve_inv_kin(x_ref)) ROS_INFO_STREAM("The RP Solution is \n" << x_ref);
+    else ROS_ERROR("RP Manager could not find solution!");
+
+    ROS_INFO("Exiting RP Test File");
+    return 0;
 }
