ec65c120602c9552838cbd628841186c67d0f956
mpollayil <mathewjosepollayil@gmail.com>
2019-06-14 17:00:21 +0200

    Found out that inside rank_update the first loop can be a trap!!! Still to be debugged! (is this the correct rank update procedure?)

--- src/reversePriorityManager.cpp
+++ src/reversePriorityManager.cpp
@@ -243,6 +243,9 @@ Eigen::MatrixXd reversePriorityManager::damped_pseudo_inv(Eigen::MatrixXd input_
 }
 
 Eigen::MatrixXd reversePriorityManager::rank_update(Eigen::MatrixXd J, Eigen::MatrixXd Jra_pinv) {
+
+    ROS_INFO("Entered RANK UPDATE!!!");
+
     // Getting the rank of J and the number of columns of Jra_pinv
     Eigen::FullPivLU<Eigen::MatrixXd> lu_decomp(J);
     auto rank_j = lu_decomp.rank();
@@ -254,8 +257,12 @@ Eigen::MatrixXd reversePriorityManager::rank_update(Eigen::MatrixXd J, Eigen::Ma
 
     // The rank update procedure (ref Matlab implementation)
     int i = 1; int j = 1;
+    ROS_INFO_STREAM("The first while stops at " << rank_j - 1 << "th iteration.");
+    ROS_INFO_STREAM("The second while stops at " << dim_jra - 1 << "th iteration.");
     while (i <= rank_j - 1) {
+        ROS_INFO_STREAM("This is the " << i << "th iteration of the first while.");
         while (j <= dim_jra - 1) {
+            ROS_INFO_STREAM("This is the " << j << "th iteration of the second while.");
             // Adding the ith column to T
             T.conservativeResize(T.rows(), T.cols() + 1);
             T.col(T.cols() - 1) = Jra_pinv.col(j);
@@ -269,15 +276,21 @@ Eigen::MatrixXd reversePriorityManager::rank_update(Eigen::MatrixXd J, Eigen::Ma
                     ROS_INFO_STREAM("Inside rank_update a new column has been added: " << i << " rank with " << j << "th column ");
                     std::cout << "T: \n" << T << std::endl;
                 }
+                ROS_INFO("Breaking");
                 i++; break;
             } else { // rank didn't change so removing column
                 T.conservativeResize(T.rows(), T.cols() - 1);
+                ROS_INFO("conservativeResize");
             }
         }
+        // TODO : Debug here!!! If j (second loop) reaches dim_jra - 1 and never breaks (so the rank of T does not increase),
+        // the code will block inside first loop (i)
     }
 
     // Checking for bounty of result
     if (T.cols() < rank_j) ROS_ERROR_STREAM("The rank update procedure did not lead to good results!");
+
+    ROS_INFO("Exiting RANK UPDATE!!!");
 
     return T;
 }
