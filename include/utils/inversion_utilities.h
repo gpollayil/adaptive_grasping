6f757bc9ca3100dffa624ae02000dcd3e25033c6
gpollayil <gpollayil@gmail.com>
2019-11-20 11:29:40 +0100

    Corrected some minor errors in implementation

--- include/utils/inversion_utilities.h
+++ include/utils/inversion_utilities.h
@@ -12,7 +12,7 @@
 * @brief This h file contains utilities for pseudo inversions
 **/
 
-inline Eigen::MatrixXd damped_pseudo_inv(Eigen::MatrixXd input_mat, double damping_coeff, double epsilon) {
+inline Eigen::MatrixXd damped_pseudo_inv(const Eigen::MatrixXd input_mat, double damping_coeff, double epsilon) {
 	// Computing the singular values
 	Eigen::JacobiSVD<Eigen::MatrixXd> svd(input_mat, Eigen::ComputeFullU | Eigen::ComputeFullV);
 	Eigen::JacobiSVD<Eigen::MatrixXd>::SingularValuesType sing_vals = svd.singularValues();
@@ -27,32 +27,33 @@ inline Eigen::MatrixXd damped_pseudo_inv(Eigen::MatrixXd input_mat, double dampi
 
 	// Changing the diagonal sv matrix
 	Eigen::MatrixXd S = input_mat; S.setZero();
-	for (int i = 0; i < sing_vals.size(); i++) {
+	for (long int i = 0; i < sing_vals.size(); i++) {
 		S(i, i) = (sing_vals(i)) / (pow(sing_vals(i), 2) + lambda_sq);
 	}
 
-	// Return the svd based damped pseudoinverse
+	// Return the svd based damped pseudo-inverse
 	return Eigen::MatrixXd(svd.matrixV() * S.transpose() * svd.matrixU().transpose());
 }
 
-inline Eigen::MatrixXd trunk_pseudo_inv(Eigen::MatrixXd input_mat, double epsilon) {
+inline Eigen::MatrixXd trunk_pseudo_inv(const Eigen::MatrixXd input_mat, double epsilon) {
 	// Computing the singular values
 	Eigen::JacobiSVD<Eigen::MatrixXd> svd(input_mat, Eigen::ComputeFullU | Eigen::ComputeFullV);
 	Eigen::JacobiSVD<Eigen::MatrixXd>::SingularValuesType sing_vals = svd.singularValues();
 
-	// Checking if the smallest sing val is really small
-	bool really_small = false;
-	double min_sing_val = sing_vals(sing_vals.size() - 1);
-	if (min_sing_val < epsilon) {
-		really_small = true;
-		ROS_WARN("The min sing val is really small! Will set it to zero for the pseudo inverse!!!");
+	// Temporarily saving and setting to zero for later modification
+	Eigen::MatrixXd S = input_mat; S.setZero();
+
+	// Checking if the smallest sing values are really small
+	for (long int i = 0; i < sing_vals.size(); i++) {
+		if (sing_vals(i) < epsilon) {
+			ROS_WARN_STREAM("The " << i << "th sing val is really small! Will set it to zero for the pseudo inverse!!!");
+			S(i, i) = 0.0;
+		} else {
+			S(i,i) = sing_vals(i);
+		}
 	}
 
-	// Changing the min sing value in the sv matrix
-	Eigen::MatrixXd S = input_mat;
-	if (really_small) S(sing_vals.size() - 1, sing_vals.size() - 1) = 0.0;
-
-	// Return the svd based damped pseudoinverse
+	// Return the svd based truncated pseudo-inverse
 	return Eigen::MatrixXd(svd.matrixV() * S.transpose() * svd.matrixU().transpose());
 }
 
